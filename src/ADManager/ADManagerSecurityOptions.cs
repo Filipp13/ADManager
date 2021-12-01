80aa05098e3128b8adbdec175760a4c9c503a961
Filipp Antipov <fantipov@deloitte.ru>
2021-12-01 11:45:04 +0300

    конструктор без параметров в опциях

--- src/ADManager/ADManagerSecurityOptions.cs
+++ src/ADManager/ADManagerSecurityOptions.cs
@@ -2,16 +2,8 @@
 {
     public sealed class ADManagerSecurityOptions
     {
-        public ADManagerSecurityOptions(
-            string login, 
-            string password)
-        {
-            Login = login;
-            Password = password;
-        }
+        public string? Login { get; set; }
 
-        public string Login { get; set; }
-
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
